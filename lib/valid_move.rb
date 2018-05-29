c5126e7b3b7e75cbf1254d1abb020ac14ea2400b
Ishani <ishaniamin@gmail.com>
2018-05-29 23:31:00 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,7 @@
 # code your #valid_move? method here
 
 require_relative"/home/nitrous/code/labs/ttt-6-position-taken-rb-q-000/lib/position_taken.rb"
+
 def valid_move?(board, position)
  position = position.to_i - 1
   if position.between?(0,8) && !position_taken?(board,position)
